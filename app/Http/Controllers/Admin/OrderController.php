d6cb69e001a23d6c86a5684610b79f66a6a6f115
maiktmp <maiktmp@gmail.com>
2019-05-28 23:04:48 -0500

    :alien: Creación del index para las compras y el detalle de las mismas

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -11,13 +11,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Cart;
 use App\Http\Controllers\Controller;
+use function foo\func;
 
 class OrderController extends Controller
 {
 
     public function index()
     {
-        $carts = Cart::with(['details', 'user'])->orderBy('id')->simplePaginate(10);
+        $carts = Cart::with(['details', 'user'])
+            ->whereHas('details', function ($q) {
+                $q->where('quantity', '<>', 0);
+            })
+            ->orderBy('id')
+            ->simplePaginate(10);
         return view('admin.order.index', ["carts" => $carts]);
     }
 
